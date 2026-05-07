6837c13943847b3cf3c951cd429a6266f47e5090
tybrianna <tybriannahall@gmail.com>
2026-05-07 14:53:44 -0400

    add coding to boost.tsx

---
+++ src/components/Boost/boost.tsx
@@ -0,0 +1,13 @@
+import "./Boost.css";
+
+const Boost = () => {
+  return (
+    <section className="boost">
+      <h2>Boost your links today</h2>
+
+      <button>Get Started</button>
+    </section>
+  );
+};
+
+export default Boost;
